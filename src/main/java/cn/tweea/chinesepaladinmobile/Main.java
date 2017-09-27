263d56ed753c2ca2b0419971695d7facf1f19201
Tweea <tweea@263.net>
2017-09-27 23:24:54 +0800

    建立 Main

---
+++ src/main/java/cn/tweea/chinesepaladinmobile/Main.java
@@ -0,0 +1,11 @@
+/*
+ * 版权所有 2017 Tweea。
+ * 保留所有权利。
+ */
+package cn.tweea.chinesepaladinmobile;
+
+public final class Main {
+	public static void main(String[] args) {
+		System.out.println("Main");
+	}
+}
