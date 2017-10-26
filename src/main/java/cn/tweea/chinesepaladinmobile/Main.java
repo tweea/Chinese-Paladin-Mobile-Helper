dd94d02ba2d1a37517a725f8eb4c2f8338ccc03b
Tweea <tweea@263.net>
2017-10-27 01:22:16 +0800

    重构

--- src/main/java/cn/tweea/chinesepaladinmobile/Main.java
+++ src/main/java/cn/tweea/chinesepaladinmobile/Main.java
@@ -20,7 +20,60 @@ public final class Main {
 		Map<String, CardDefinition> definitions = DataFiles.loadCardDefinition(new ClassPathResource("CardList.xlsx"), grades);
 		Map<String, Card> cards = DataFiles.loadCard(new ClassPathResource("CardStatus.xlsx"), definitions);
 
+		Map<String, Map<String, Integer>> needsMap = computeNeed(cards, levelNeedMap);
+		for (Map.Entry<String, Map<String, Integer>> needsEntry : needsMap.entrySet()) {
+			String name = needsEntry.getKey();
+			Map<String, Integer> needs = needsEntry.getValue();
+			System.out.println(name + ':' + needs);
+		}
+	}
+
+	private static Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> buildLevelNeedMap() {
+		Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> levelNeedMap = new EnumMap<>(CardLevelType.class);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.聚魂, null, CardUpgradeNeedType.聚魂);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.修炼, null, CardUpgradeNeedType.修炼);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.飞升, null, CardUpgradeNeedType.飞升);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘1, CardDependencyType.仙缘1, CardUpgradeNeedType.仙缘);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘2, CardDependencyType.仙缘2, CardUpgradeNeedType.仙缘);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘31, CardDependencyType.仙缘31, CardUpgradeNeedType.仙缘);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘32, CardDependencyType.仙缘32, CardUpgradeNeedType.仙缘);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘33, CardDependencyType.仙缘33, CardUpgradeNeedType.仙缘);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘34, CardDependencyType.仙缘34, CardUpgradeNeedType.仙缘);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技1, null, CardUpgradeNeedType.绝技一);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技1, CardDependencyType.绝技1, CardUpgradeNeedType.绝技一副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技2, null, CardUpgradeNeedType.绝技二);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技2, CardDependencyType.绝技2, CardUpgradeNeedType.绝技二副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技2, CardDependencyType.绝技3, CardUpgradeNeedType.绝技二副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, null, CardUpgradeNeedType.绝技三);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, CardDependencyType.绝技1, CardUpgradeNeedType.绝技三副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, CardDependencyType.绝技2, CardUpgradeNeedType.绝技三副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, CardDependencyType.绝技3, CardUpgradeNeedType.绝技三副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技4, null, CardUpgradeNeedType.绝技四);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技5, null, CardUpgradeNeedType.绝技五);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, null, CardUpgradeNeedType.绝技六);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, CardDependencyType.绝技1, CardUpgradeNeedType.绝技六副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, CardDependencyType.绝技2, CardUpgradeNeedType.绝技六副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, CardDependencyType.绝技3, CardUpgradeNeedType.绝技六副);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.云裳1, null, CardUpgradeNeedType.云裳);
+		buildLevelNeedMap(levelNeedMap, CardLevelType.云裳2, null, CardUpgradeNeedType.云裳);
+		return levelNeedMap;
+	}
+
+	private static void buildLevelNeedMap(Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> levelNeedMap, CardLevelType levelType,
+		CardDependencyType dependencyType, CardUpgradeNeedType upgradeNeedType) {
+		List<Pair<CardDependencyType, CardUpgradeNeedType>> levelNeedList = levelNeedMap.get(levelType);
+		if (levelNeedList == null) {
+			levelNeedList = new ArrayList<>();
+			levelNeedMap.put(levelType, levelNeedList);
+		}
+
+		levelNeedList.add(Pair.of(dependencyType, upgradeNeedType));
+	}
+
+	private static Map<String, Map<String, Integer>> computeNeed(Map<String, Card> cards,
+		Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> levelNeedMap) {
 		Map<String, Map<String, Integer>> needsMap = new LinkedHashMap<>();
+
 		for (String name : cards.keySet()) {
 			Map<String, Integer> needs = new LinkedHashMap<>();
 			needs.put("总计", 0);
@@ -83,53 +136,8 @@ public final class Main {
 				}
 			}
 		}
-		for (Map.Entry<String, Map<String, Integer>> needsEntry : needsMap.entrySet()) {
-			String name = needsEntry.getKey();
-			Map<String, Integer> needs = needsEntry.getValue();
-			System.out.println(name + ':' + needs);
-		}
-	}
 
-	private static Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> buildLevelNeedMap() {
-		Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> levelNeedMap = new EnumMap<>(CardLevelType.class);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.聚魂, null, CardUpgradeNeedType.聚魂);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.修炼, null, CardUpgradeNeedType.修炼);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.飞升, null, CardUpgradeNeedType.飞升);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘1, CardDependencyType.仙缘1, CardUpgradeNeedType.仙缘);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘2, CardDependencyType.仙缘2, CardUpgradeNeedType.仙缘);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘31, CardDependencyType.仙缘31, CardUpgradeNeedType.仙缘);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘32, CardDependencyType.仙缘32, CardUpgradeNeedType.仙缘);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘33, CardDependencyType.仙缘33, CardUpgradeNeedType.仙缘);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.仙缘34, CardDependencyType.仙缘34, CardUpgradeNeedType.仙缘);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技1, null, CardUpgradeNeedType.绝技一);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技1, CardDependencyType.绝技1, CardUpgradeNeedType.绝技一副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技2, null, CardUpgradeNeedType.绝技二);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技2, CardDependencyType.绝技2, CardUpgradeNeedType.绝技二副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技2, CardDependencyType.绝技3, CardUpgradeNeedType.绝技二副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, null, CardUpgradeNeedType.绝技三);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, CardDependencyType.绝技1, CardUpgradeNeedType.绝技三副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, CardDependencyType.绝技2, CardUpgradeNeedType.绝技三副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技3, CardDependencyType.绝技3, CardUpgradeNeedType.绝技三副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技4, null, CardUpgradeNeedType.绝技四);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技5, null, CardUpgradeNeedType.绝技五);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, null, CardUpgradeNeedType.绝技六);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, CardDependencyType.绝技1, CardUpgradeNeedType.绝技六副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, CardDependencyType.绝技2, CardUpgradeNeedType.绝技六副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.绝技6, CardDependencyType.绝技3, CardUpgradeNeedType.绝技六副);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.云裳1, null, CardUpgradeNeedType.云裳);
-		buildLevelNeedMap(levelNeedMap, CardLevelType.云裳2, null, CardUpgradeNeedType.云裳);
-		return levelNeedMap;
-	}
-
-	private static void buildLevelNeedMap(Map<CardLevelType, List<Pair<CardDependencyType, CardUpgradeNeedType>>> levelNeedMap, CardLevelType levelType,
-		CardDependencyType dependencyType, CardUpgradeNeedType upgradeNeedType) {
-		List<Pair<CardDependencyType, CardUpgradeNeedType>> levelNeedList = levelNeedMap.get(levelType);
-		if (levelNeedList == null) {
-			levelNeedList = new ArrayList<>();
-			levelNeedMap.put(levelType, levelNeedList);
-		}
-
-		levelNeedList.add(Pair.of(dependencyType, upgradeNeedType));
+		return needsMap;
 	}
 
 	private static void accumulateNeed(Map<String, Integer> needs, String name, int need) {
